71b91cccda9a5f063ce84236c45cc02a43bde1d0
Taschner <admin@NinjaGaming.gg>
2024-08-24 22:53:45 +0200

    Refactor PlayerItemConsumeListener to Kotlin

--- src/main/java/de/cyne/advancedlobby/AdvancedLobby.java
+++ src/main/java/de/cyne/advancedlobby/AdvancedLobby.java
@@ -176,7 +176,7 @@ public class AdvancedLobby extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new PlayerFishListener(), AdvancedLobby.getInstance());
         Bukkit.getPluginManager().registerEvents(new PlayerInteractEntityListener(), AdvancedLobby.getInstance());
         Bukkit.getPluginManager().registerEvents(new PlayerInteractEventListener(), AdvancedLobby.getInstance());
-        Bukkit.getPluginManager().registerEvents(new PlayerItemConsumeListener(), AdvancedLobby.getInstance());
+        Bukkit.getPluginManager().registerEvents(new PlayerItemConsumeEventListener(), AdvancedLobby.getInstance());
         Bukkit.getPluginManager().registerEvents(new PlayerItemDamageEventListener(), AdvancedLobby.getInstance());
         Bukkit.getPluginManager().registerEvents(new PlayerItemHeldEventListener(), AdvancedLobby.getInstance());
         Bukkit.getPluginManager().registerEvents(new PlayerJoinEventListener(), AdvancedLobby.getInstance());
