1f690a3937e5a93fc1e841df6e9730475e845d00
Taschner <admin@NinjaGaming.gg>
2024-08-25 12:06:16 +0200

    Refactor GlobalMuteCommand to Kotlin

---
+++ src/main/java/gg/ninjagaming/advancedlobby/commands/GlobalMuteCommand.kt
@@ -0,0 +1,49 @@
+package gg.ninjagaming.advancedlobby.commands
+
+import de.cyne.advancedlobby.AdvancedLobby
+import de.cyne.advancedlobby.locale.Locale
+import org.bukkit.Bukkit
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+class GlobalMuteCommand : CommandExecutor {
+    override fun onCommand(sender: CommandSender, cmd: Command, label: String, args: Array<String>): Boolean {
+        if (sender !is Player) {
+            sender.sendMessage("§cThis command is available for players only.")
+            return true
+        }
+
+        if (!sender.hasPermission("advancedlobby.commands.globalmute")) {
+            sender.sendMessage(Locale.NO_PERMISSION.getMessage(sender))
+            return true
+        }
+
+        if (!AdvancedLobby.globalMute) {
+            AdvancedLobby.globalMute = true
+            for (players in Bukkit.getOnlinePlayers()) {
+                players.sendMessage(
+                    Locale.COMMAND_GLOBALMUTE_ENABLE.getMessage(sender).replace(
+                        "%player%", AdvancedLobby.getName(
+                            sender
+                        )
+                    )
+                )
+            }
+            return true
+        }
+
+        AdvancedLobby.globalMute = false
+        for (players in Bukkit.getOnlinePlayers()) {
+            players.sendMessage(
+                Locale.COMMAND_GLOBALMUTE_DISABLE.getMessage(sender).replace(
+                    "%player%", AdvancedLobby.getName(
+                        sender
+                    )
+                )
+            )
+        }
+        return true
+    }
+}
