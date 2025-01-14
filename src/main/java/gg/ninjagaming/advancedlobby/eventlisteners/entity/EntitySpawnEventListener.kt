5f9e35b34436b4412cf855e091a1928c25c73516
Taschner <admin@NinjaGaming.gg>
2025-01-14 20:53:57 +0100

    Refactor entity spawn handling to improve clarity.
    Separated checks for spawn reason and entity type for better readability. Added early return for custom spawn reasons to reduce nesting and improve code maintainability.

--- src/main/java/gg/ninjagaming/advancedlobby/eventlisteners/entity/EntitySpawnEventListener.kt
+++ src/main/java/gg/ninjagaming/advancedlobby/eventlisteners/entity/EntitySpawnEventListener.kt
@@ -1,5 +1,8 @@
 package gg.ninjagaming.advancedlobby.eventlisteners.entity
 
+import net.kyori.adventure.text.Component
+import org.bukkit.Bukkit
+import org.bukkit.entity.Bat
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -9,9 +12,14 @@ import org.bukkit.event.entity.EntitySpawnEvent
 object EntitySpawnEventListener: Listener {
     @EventHandler
     fun onEntitySpawn(event: EntitySpawnEvent) {
-        if (event.entity.entitySpawnReason != SpawnReason.CUSTOM || event.entity !is Player){
-            event.isCancelled = true
-        }
+        val entity = event.entity
 
+        if (entity.entitySpawnReason == SpawnReason.CUSTOM)
+            return
+
+        if (entity !is Player){
+            event.isCancelled = true
+            return
+        }
     }
 }
